1ac940c79d339d076449e1599e458c062a603cfb
Newton Kyari <ntkyari@gmail.com>
2025-05-29 10:47:27 +0100

    Expose Swagger UI

--- FcmMessaging.Api/Program.cs
+++ FcmMessaging.Api/Program.cs
@@ -28,11 +28,15 @@ using (var scope = app.Services.CreateScope())
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
